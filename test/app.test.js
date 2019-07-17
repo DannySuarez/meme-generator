252cb97ab000ca307d556b46410c3f59f55d552e
DannySuarez <suarezd10@gmail.com>
2019-07-16 17:37:09 -0700

    finished routes and testing

--- test/app.test.js
+++ test/app.test.js
@@ -33,6 +33,7 @@ describe('app routes', () => {
         });
       });
   });
+
   it('gets a meme', async() => {
     const meme = await Meme.create({ top: 'A meme', image: 'url string', bottom: 'generator' });
     return request(app)
@@ -42,9 +43,9 @@ describe('app routes', () => {
         expect(res.body).toEqual([memeJSON]);
       });
   });
+
   it('gets a meme from id', async() => {
     const meme = await Meme.create({ top: 'meme' });
-
     return request(app)
       .get(`/api/v1/memes/${meme._id}`)
       .then(res => {
@@ -53,6 +54,25 @@ describe('app routes', () => {
           top: 'meme',
           __v: 0
         });
+      });
+  });
+
+  it('uses put', async() => {
+    const meme = await Meme.create({ top: 'another one', image: 'something' });
+    return request(app)
+      .put(`/api/v1/memes/${meme._id}`)
+      .send({ image: 'url here' })
+      .then(res => {
+        expect(res.body.image).toEqual('url here');
+      });
+  });
+
+  it('deletes', async() => {
+    const meme = await Meme.create({ top: 'I am a meme', image: 'url', bottom: 'A good meme' });
+    return request(app)
+      .delete(`/api/v1/memes/${meme._id}`)
+      .then(res => {
+        expect(res.body.top).toEqual('I am a meme');
       });
   });
 
